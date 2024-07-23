7fb9a7969d80b75f6cc8a50af2fd19714a945e8d
Michel Pereira <pereira.michel@gmail.com>
2024-07-23 16:41:15 -0300

    Término do projeto conta corrente, da trilha java básico

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal{
 
     public static void main(String... args) throws Exception{
-        System.out.println("Hello Word");
+        //TODO:Conhecer e importar a classe Scanner
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        String convertSaldo;
+        String saldoComVirgula;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCliente = scan.next();
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scan.next();
+        System.out.println("Por favor, digite a sua conta: ");
+        numero = scan.nextInt();
+        System.out.println("Por favor, digite o seu saldo: ");
+        convertSaldo = scan.next();
+
+        saldoComVirgula = convertSaldo.replace(".", ",");
+
+        saldo = Double.parseDouble(convertSaldo.replace(",", ".")); // caso queira efetuar cálculos
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$ " + saldoComVirgula +" já está disponível para saque");
+
     }
 
 
